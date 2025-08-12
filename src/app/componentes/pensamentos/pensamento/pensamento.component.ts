f4dd7f495796e03f151f2fe44ea7affcc0b6ad83
mjoliveir <32xxthunderxx32@gmail.com>
2025-08-12 14:09:03 -0300

    Add PensamentoComponent with HTML, CSS, and unit tests; implement dynamic content rendering

--- src/app/componentes/pensamentos/pensamento/pensamento.component.ts
+++ src/app/componentes/pensamentos/pensamento/pensamento.component.ts
@@ -1,29 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component,Input, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-listar-pensamentos',
-  templateUrl: './listar-pensamentos.component.html',
-  styleUrls: ['./listar-pensamentos.component.css']
+  selector: 'app-pensamento',
+  templateUrl: './pensamento.component.html',
+  styleUrls: ['./pensamento.component.css']
 })
-export class ListarPensamentosComponent implements OnInit {
+export class PensamentoComponent implements OnInit {
 
-  listaPensamentos = [
-    {
+  @Input() pensamento = {
     conteudo: `teshbknkbknnsadkcxvkjsd fvkjsdfkj nvkjds vj fj fj vn fvn fn vf nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnte`,
-    autoria: `teste`,
-    modelo: `modelo1`
-  },
-  {
-    conteudo: `teshnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnte`,
     autoria: `teste`,
     modelo: `modelo1`
   }
 
-
-  ];
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  larguraPensamento(): string {
+    if(this.pensamento.conteudo.length >= 256){
+      return `pensamento-g`
+    } return `pensamento-p`
+  }
 }
